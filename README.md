978636556696e78357e0d202cb129c0e156b0982
Николай Тюнис <niksan540@gmail.com>
2022-02-04 15:07:44 +0300

    8

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Nikolas888/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/Nikolas888/frontend-project-lvl1/actions)
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 ![example workflow](https://github.com/Nikolas888/frontend-project-lvl1/actions/workflows/test.yml/badge.svg)
+
+https://asciinema.org/a/MynzoGSby4Pbyf9RFN9jRdPdf
---
+++ bin/brain-even.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const getRandomFloat = () => {
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log ('Hello ' + name + '!');
+  console.log('');
+  const sory = '\n\'yes\' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ' + name + '!';
+  const sory2 = '\n\'no\' is wrong answer ;(. Correct answer was \'yes\'.\nLet\'s try again, ' + name + '!';
+  let n = 1;
+
+
+  while (n <= 3) {
+
+     const num = Math.floor(Math.random() * 30);
+     console.log ('Question: ' + num);
+     const answer = readlineSync.question('Your answer: ');
+
+  if (num % 2 == 0 && answer == 'yes' && n <= 3) {
+     console.log('Correct!');
+     n += 1;
+
+  }
+  else if (num % 2 !== 0 && answer == 'no' && n <= 3) {
+     console.log('Correct!');
+     n += 1;
+
+  }
+  else if (num % 2 == 0 && answer !== 'yes') {
+     n += 4;
+     console.log(sory2);
+  }
+  else if (num % 2 !== 0 && answer !== 'no') {
+     console.log(sory);
+     n += 4;
+  }
+};
+
+if (n === 4) {
+console.log('Congratulations, ' + name + '!');
+}
+};
+
+console.log('\nWelcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+console.log('');
+getRandomFloat();
+
+export default getRandomFloat;
