9be8b735b65403171d5b97408456c090e2c34a83
Николай Тюнис <niksan540@gmail.com>
2022-02-09 15:41:30 +0300

    18

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const hello = () => {
   const name = readlineSync.question('May I have your name?');
-  const hi = 'Hello';
+  const hi = 'Hello,';
   const space = ' ';
   const exclamation = '!';
   console.log(hi + space + name);
