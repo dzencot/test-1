36447e39acb61979953afc1ea004f7e2a2b87b70
Николай Тюнис <niksan540@gmail.com>
2022-02-09 13:47:17 +0300

    14

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const hello = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   const hi = 'Hello';
   const space = ' ';
