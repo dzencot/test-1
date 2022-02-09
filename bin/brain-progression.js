1f1ca45104df57be63b4bb5b5f613faa7e235184
Николай Тюнис <niksan540@gmail.com>
2022-02-09 16:47:54 +0300

    20

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -2,102 +2,87 @@
 import readlineSync from 'readline-sync';
 
 const getRandomFloat = () => {
-
   const name = readlineSync.question('May I have your name? ');
-  console.log ('Hello ' + name + '!');
+  const hello = 'Hello ';
+  const ex = '!';
+  const space = ' ';
+  const question = 'Question: ';
+  console.log(hello + name + ex);
   console.log('');
-  let num4 = 1; //результат
-  let n = 1; //раунды
-
+  let num4 = 1; // результат
+  let n = 1; // раунды
 
   while (n <= 3) {
+    const num = Math.floor(Math.random() * 30);
+    const num2 = Math.floor(Math.random() * 30);
+    const num3 = Math.floor(Math.random() * 9);
 
-     const num = Math.floor(Math.random() * 30);
-     const num2 = Math.floor(Math.random() * 30);
-     const num3 = Math.floor(Math.random() * 9);
-
-     const fn = (num, num2) => {
-       let i = 0;
-       let str = '';
-       while (i <= 9){
+    const fn = (a, b) => {
+      let i = 0;
+      let str = '';
+      while (i <= 9) {
         if (i !== num3) {
-         str = str + num + ' ';
-         num += num2;
-         i += 1;
-       }
-       else if (i === num3){
-         str = str + ".." + ' ';
-         num += num2;
-         i += 1;
-       };
-       };
+          str = str + a + space;
+          a += b;
+          i += 1;
+        } else if (i === num3) {
+          str += '.. ';
+          a += b;
+          i += 1;
+        }
+      }
       return str;
-     };
+    };
 
+    const otvet = () => { // функция вычисляет результат num4
+      if (num3 === 0) {
+        num4 = num;
+      } else if (num3 === 1) {
+        num4 = num + num2;
+      } else if (num3 === 2) {
+        num4 = num + num2 + num2;
+      } else if (num3 === 3) {
+        num4 = num + num2 + num2 + num2;
+      } else if (num3 === 4) {
+        num4 = num + num2 + num2 + num2 + num2;
+      } else if (num3 === 5) {
+        num4 = num + num2 + num2 + num2 + num2 + num2;
+      } else if (num3 === 6) {
+        num4 = num + num2 + num2 + num2 + num2 + num2 + num2;
+      } else if (num3 === 7) {
+        num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2;
+      } else if (num3 === 8) {
+        num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2;
+      } else if (num3 === 9) {
+        num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2;
+      } else if (num3 === 10) {
+        num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2;
+      }
+      return num4;
+    };
 
-const otvet = () => { // функция вычисляет результат num4
-  if (num3 === 0){
-    num4 = num;
-  }
-  else if (num3 === 1){
-    num4 = num + num2;
-  }
-  else if (num3 === 2){
-    num4 = num + num2 + num2;
-  }
-  else if (num3 === 3){
-    num4 = num + num2 + num2 + num2;
-  }
-  else if (num3 === 4){
-    num4 = num + num2 + num2 + num2 + num2;
-  }
-  else if (num3 === 5){
-    num4 = num + num2 + num2 + num2 + num2 + num2;
-  }
-  else if (num3 === 6){
-    num4 = num + num2 + num2 + num2 + num2 + num2 + num2;
-  }
-  else if (num3 === 7){
-    num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2;
-  }
-  else if (num3 === 8){
-    num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2;
-  }
-  else if (num3 === 9){
-    num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2;
-  }
-  else if (num3 === 10){
-    num4 = num + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2 + num2;
-  }
-  return num4;
-} ;
+    otvet();
 
-otvet();
+    const quest = () => {
+      console.log(question + fn(num, num2));
+    };
 
+    quest();
 
-const quest = () => {
+    const answer = readlineSync.question('Your answer: ');
 
-    console.log('Question: ' + fn(num, num2));
- };
-
-quest();
-
-
-  const answer = readlineSync.question('Your answer: ');
-
-  if ( answer == num4 && n <= 3) {
-     console.log('Correct!');
-     n += 1;
+    if (answer == num4 && n <= 3) {
+      console.log('Correct!');
+      n += 1;
+    } else if (answer !== num3) {
+      n += 4;
+      console.log('\'' + answer + '\' is wrong answer ;(. Correct answer was \'' + num4 + '\' . Let\'s try again, ' + name + '!');
+    }
   }
-  else if (answer !== num3) {
-     n += 4;
-     console.log('\'' + answer + '\' is wrong answer ;(. Correct answer was \'' + num4 + '\' . Let\'s try again, ' + name + '!');
-  }
-};
 
-if (n === 4) {
-console.log('Congratulations, ' + name + '!');
-}
+  if (n === 4) {
+    console.log('Congratulations, ' + name + '!');
+  }
 };
 
 console.log('\nWelcome to the Brain Games!');
--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,6 @@ const hello = () => {
   const name = readlineSync.question('May I have your name?');
   const hi = 'Hello,';
   const space = ' ';
-  const exclamation = '!';
   console.log(hi + space + name);
 };
 
